ac461d1bbc6f691433a22080e0c01735ab08edb9
shakilshagu <muhammadshakil1968@gmail.com>
2023-11-18 17:06:05 +0530

    newcommit

--- src/app/components/input.jsx
+++ src/app/components/input.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-const Input = ({ label,type,id }) => {
+const Input = ({ label, type, id }) => {
   return (
     <div className="mb-4">
-      <label htmlFor={id}  className="block  text-gray-600 font-semibold mb-2"  >{label}</label>
-      <input type={type} id={id} name={id} autoComplete="off" className="w-ful p-2"/>
+      <label htmlFor={id} className="block  text-gray-600 font-semibold mb-2">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={id}
+        name={id}
+        autoComplete="off"
+        className="w-ful p-2 rounded-md border border-orange-700  "
+      />
     </div>
   );
 };
